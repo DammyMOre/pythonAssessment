bf316df323cf602b44df606229066db37f205e9d
DammyMOre <famuyiwadammya@gmail.com>
2024-09-02 02:29:08 +0100

    pizzawahala.py

--- Assignment/pizzawahala.py
+++ Assignment/pizzawahala.py
@@ -4,6 +4,15 @@ create a menu list for the pizza type, number of slices and price per box
 print out the menu list
 collect the number of guest
 collect the pizza type
+if case sapa size
+print number_of_guess /4
+if case small money
+print number_of_guess /6
+if case bigboys
+print number_of_guess /8
+if case odogwu
+print number_of_guess /12
+
 '''
 
 
@@ -25,8 +34,57 @@ Odogwu                 12                 4200
 """
 print(menu_list)
 
+
+
 number_of_guest = int(input("Enter number of guest: "))
 
-pizza_type = int(input("pizza type: "))
+pizza_type = input("pizza type: ")
 
-#number of boxes to buy = 
+match pizza_type:
+
+	case "sapasize":
+
+		print("boxes to buy is: " ,number_of_guest /4)
+
+		print("remaining slices of pizza is: ", 4 - number_of_guest)
+
+		print("your money is: ", 2000)
+ 
+	case "small_money":
+
+		print("boxes to buy is: ",number_of_guest /6)
+
+		print("remaining slices of pizza is: " ,6 - number_of_guest)
+
+		print("your money is: ", 2400)
+
+	case "big_boys":
+
+		print("boxes to buy is: ", number_of_guest /8)
+
+		print("remaining slices of pizza is: ",8 -number_of_guest)
+
+		print("your money is: ", 3000)
+
+
+	case "odogwu":
+
+		boxestobuy = number_of_guest /12
+
+		print("boxes to buy is: ", number_of_guest /12)
+
+if (number_of_guest>12):
+
+	print("remaining slices of pizza is: ",number_of_guest-12)
+
+elif (number_of_guest<=12):
+
+		print("remaining slices of pizza is: ",12 - number_of_guest)
+
+
+		print ("your money is: ", 4200)
+
+if (number_of_guest > 12):
+	 	print ("#", boxestobuy *4200)
+	
+

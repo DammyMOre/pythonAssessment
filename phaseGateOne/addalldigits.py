b304a4fbe59ebd49913df5a806daed3b623387f0
DammyMOre <famuyiwadammya@gmail.com>
2024-09-30 15:50:37 +0100

    addalldigits.py

---
+++ phaseGateOne/addalldigits.py
@@ -0,0 +1,13 @@
+"""
+pseudo code
+collect an integer between 0 and 1000
+save as digit
+sum the digits 
+print result
+"""
+
+digit1 = int(input("enter a digit: "))
+digit2 = int(input("enter a digit: "))
+digit3 = int(input("enter a digit: "))
+sum = digit1 + digit2 + digit3
+print(sum) 

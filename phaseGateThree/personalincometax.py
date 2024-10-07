0d25a1335db73c6cfe985eeed44297f7c310cdcd
DammyMOre <famuyiwadammya@gmail.com>
2024-10-07 17:10:50 +0100

    personalincometax.py

---
+++ phaseGateThree/personalincometax.py
@@ -0,0 +1,10 @@
+status = """
+0. single filers
+1. married filling jointly
+2. married filling seperately
+3. head of household
+"""
+
+print (status)
+filling_status = float(input("enter your status"))
+

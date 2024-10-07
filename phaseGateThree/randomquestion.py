ad64ef9dfeb493e5fc2d71bc4d91eb4bda59ab2d
DammyMOre <famuyiwadammya@gmail.com>
2024-10-07 17:00:48 +0100

    randomquestion.py

---
+++ phaseGateThree/randomquestion.py
@@ -0,0 +1,37 @@
+import random
+
+answer = 0
+correct = 0
+wrong = 0
+for times in range(1,4,1):
+	random_number1 = random.randint(1,10)	
+
+	random_number2 = random.randint(1,10)
+
+	print(random_number1)	
+	print(random_number2)
+
+	answer = int(input("Whats the difference between number 1 and number 2: "))
+
+	correct_answer = random_number1 - random_number2
+
+	if correct_answer < 0: 
+		correct_answer = random_number2 - random_number1 
+
+	if answer == correct_answer:
+		correct = times - wrong
+		print("correct")
+	
+	if answer != correct_answer:
+		wrong = times - wrong
+		print("you are wrong")
+	
+ 
+#final_score1 = correct
+#final_score2 = wrong
+
+if correct > wrong:
+	print("Your final score is: ", correct)
+
+else:
+	print("Your final score is: ", wrong)

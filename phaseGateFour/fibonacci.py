f2b0c9400e79aee4667ee9f66a9151f11e4a8315
DammyMOre <famuyiwadammya@gmail.com>
2024-10-10 19:01:23 +0100

    phaseGateFour

---
+++ phaseGateFour/fibonacci.py
@@ -0,0 +1,4 @@
+sum =0
+for number in range(0,20,1):
+	sum+=number
+	print(sum)
---
+++ phaseGateFour/triangle.py
@@ -0,0 +1,6 @@
+
+for number in range (1,8,1):
+	for numb in range (1, number,2):
+		print(numb, end=" * ")		
+	print()
+
